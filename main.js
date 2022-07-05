512fbf295740a9f354669f06741fa88aa62e862d
asanebou.mackey <asanebou.job4219@gmail.com>
2022-07-05 18:05:55 +0900

    電卓作成

--- main.js
+++ main.js
@@ -1,15 +1,57 @@
-const btn = document.querySelectorAll('#btn');
-const result = document.querySelector('#result');
-console.log(result);
+let number = document.querySelectorAll('button');
+let result = document.querySelector('#result');
 
-//console.log(btn[2]);
+let operator = document.querySelector('#operator');
 
-/*function click1() {
-    console.log(btn[0]);
-}*/
-
-for(let i =0; i < btn.length; i++) {
-    btn[i].addEventListener("click", function() {
-        console.log(btn[i]);
-    }, false);
+function calc(button) {
+    number = button.innerHTML;
+    if(number == 'c') {
+        result.value = 0;
+        //result.innerHTML = '0';
+    } else if(number == '=') {
+        result.value = eval(result.value);
+        //result.innerHTML = eval(result.innerHTML);
+    } else {
+        if(result.value == '0') {
+            result.value = number;
+        } else {
+            result.value += number;
+        }
+    }
 }
+
+
+function clickOperator() {
+    operator = button.innerHTML;
+    console.log(operator);
+}
+
+
+
+
+
+
+
+
+//<input type="text" id="result" value=""> id="result">< 
+
+/*
+function calc(button) {
+    number = button.innerHTML;
+    if(number == 'c') {
+        result.innerHTML = '0';
+    } else if(number == '=') {
+        result.innerHTML = eval(result.innerHTML);
+    } else {
+        if(result.value == '0') {
+            result.value = number;
+        } else {
+            if(result.innerHTML == '0') {
+                result.innerHTML = number;
+            } else {
+                result.innerHTML += number;
+            }
+        }
+    }
+}
+*/
