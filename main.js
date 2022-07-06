1522cbd186cf18a2733676a695bae10f2a23f7ec
asanebou.mackey <asanebou.job4219@gmail.com>
2022-07-06 17:52:10 +0900

    電卓作成中

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
+
 let number = document.querySelectorAll('button');
+
 let result = document.querySelector('#result');
 
 let operator = document.querySelector('#operator');
 
+//数字(0〜9)ボタンと"c"、"="ボタンを押した時に使う関数
 function calc(button) {
     number = button.innerHTML;
     if(number == 'c') {
         result.value = 0;
-        //result.innerHTML = '0';
     } else if(number == '=') {
         result.value = eval(result.value);
-        //result.innerHTML = eval(result.innerHTML);
     } else {
         if(result.value == '0') {
             result.value = number;
@@ -20,10 +21,10 @@ function calc(button) {
     }
 }
 
-
-function clickOperator() {
-    operator = button.innerHTML;
-    console.log(operator);
+//演算子(+, -, ×, ÷)ボタンを押した時に使う関数
+function clickOperator(button) {
+    operator = button.value;
+    result.value += operator;
 }
 
 
